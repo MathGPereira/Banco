6ffb0d899600e7544d80e1472bd2fa334e866b4b
Matheus Pereira <matheusgp.mto@outlook.com>
2023-05-04 10:44:17 -0300

    Criando o parâmetros da classe Cliente e iniciando o desenvolvimento da classe Conta

--- servidor/js/classes/Cliente.js
+++ servidor/js/classes/Cliente.js
@@ -1,10 +1,21 @@
 export default class Cliente {
 
+    #nome;
+    #sobrenome;
+    #email;
+    #conta;
+
+    constructor(nome, sobrenome, email, conta) {
+        this.#nome = nome;
+        this.#sobrenome = sobrenome;
+        this.#email = email;
+    }
+
     login() {
 
     }
 
-    sacar() {
+    sacar(valor) {
 
     }
 
@@ -16,7 +27,10 @@ export default class Cliente {
         
     }
 
-    static async getAPI() {
-        
+    static async #getAPI() {
+        const url = `http://localhost:1337/api/${tipo}/${id}`;
+        const resp = await fetch(url);
+
+        return resp.json();
     }
 }
---
+++ servidor/js/classes/Conta.js
@@ -0,0 +1,7 @@
+export class Conta {
+    
+    #usuario;
+    #senha;
+    #cliente;
+    #valor;
+}
